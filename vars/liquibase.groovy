5afb20ba10b4b60cf2f4d238c2a9be7be1405113
venu434 <venugopal.8593@gmail.com>
2022-09-25 14:14:27 +0530

    thb

--- vars/liquibase.groovy
+++ vars/liquibase.groovy
@@ -1,6 +1,37 @@
-#!/usr/bin/env groovy
+import hudson.tasks.test.AbstractTestResultAction
+import hudson.model.Actionable
+import java.text.SimpleDateFormat
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
-}
+def call(body) {
 
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    def branch = "${BRANCH_NAME}"
+    buildId = "${BUILD_ID}"
+    def dateFormat = new SimpleDateFormat("yyyyMMddHHmm")
+    def date = dateFormat.format(new Date())
+    def FAILED_STAGE = 'none'
+
+    def branchName = config.branchName
+    def gitRepoUrl = config.gitRepoUrl
+ 
+ def test = [
+               age : config.age,
+                ]   
+
+pipeline {
+agent {
+      label "master"
+    }
+  stages {  
+  stage ('checking config') {
+      steps {
+          echo "stage"
+          echo "AGE= ${age}"
+        } // steps
+  } //End of stage
+  } //End of stages
+} //End of pipeline       
+}//End of body
