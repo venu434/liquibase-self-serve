124bad997feca831175b0d133cc73a2da8d24a71
venu434 <96043084+venu434@users.noreply.github.com>
2022-09-25 13:27:46 +0530

    Update liquibase.groovy

--- vars/liquibase.groovy
+++ vars/liquibase.groovy
@@ -1,38 +1,8 @@
-import hudson.tasks.test.AbstractTestResultAction
-import hudson.model.Actionable
-import java.text.SimpleDateFormat
+#!/usr/bin/env groovy
 
-def call(body) {
+def call(String name = 'human') {
+  echo "Hello, ${name}."
+} 
 
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
-    def branch = "${BRANCH_NAME}"
-    buildId = "${BUILD_ID}"
-    def dateFormat = new SimpleDateFormat("yyyyMMddHHmm")
-    def date = dateFormat.format(new Date())
-    def FAILED_STAGE = 'none'
 
-    def branchName = config.branchName
-    def gitRepoUrl = config.gitRepoUrl
- 
- def test = [
-               age : config.age,
-                ]   
-
-pipeline {
-agent {
-      label "master"
-    }
-  stages {  
-  stage ('checking config') {
-      steps {
-          echo "stage"
-          echo "AGE= ${age}"
-        } // steps
-  } // stage
-  } // stages
-} // pipeline       
-}//End of body
 
